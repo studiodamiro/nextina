4299f14de7856c3bc960634c1a66cd3a59354de0
damiro <hello.damiro@gmail.com>
2023-10-15 13:06:13 +0800

    rebase tina config

--- tina/config.ts
+++ tina/config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'tinacms';
 
+const branch = process.env.TINA_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD;
+
 export default defineConfig({
   branch: process.env.VERCEL_GIT_COMMIT_REF,
   clientId: process.env.TINA_CLIENT_ID || '',
