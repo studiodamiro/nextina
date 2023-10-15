7a5b90d648ca8d79d880636f6fc1e23389c91a5c
damiro <hello.damiro@gmail.com>
2023-10-16 00:43:33 +0800

    checkpoint

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import client from '@/tina/__generated__/client';
 import PostList from '@/components/app/post-list';
 
-export default async function PostPage() {
+export default async function HomePage() {
   const result = await client.queries.postConnection();
   return <PostList {...result} />;
 }
--- tina/config.ts
+++ tina/config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'tinacms';
 
-const branch = process.env.TINA_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD;
-
 export default defineConfig({
-  branch: branch,
+  branch: 'main',
   clientId: process.env.TINA_CLIENT_ID || '',
   token: process.env.TINA_TOKEN || '',
   build: { outputFolder: 'admin', publicFolder: 'public' },
